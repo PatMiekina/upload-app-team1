d6f548a22ee9b5f64e7c37a26f48db59e263d87d
Patrycja Miekina <patrycja.miekina@madetech.com>
2022-08-09 16:05:06 +0100

    changed port to heroku port process.env.PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, 'views/index.html'))
@@ -9,6 +9,6 @@ res.sendFile(path.join(__dirname, 'views/index.html'))
 
 app.use(express.static(path.join(__dirname, './public')))
 
-app.listen(PORT, () => {
-console.log(`App running on port ${PORT} http://localhost:3000`)
+app.listen(port, () => {
+console.log(`App running on port ${port} http://localhost:3000`)
 })
