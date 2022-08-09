e5616dae86d16666a1015ea0293fd593c8a855a0
Patrycja Miekina <patrycja.miekina@madetech.com>
2022-08-09 15:48:32 +0100

    Changed the app's port to match Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = 3000
+const PORT = process.env.port || 3000
 
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, 'views/index.html'))
