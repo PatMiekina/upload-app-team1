6b90acc32e4c2be25b7f2601ed63e6fea86cf1b8
Donny <donny.hyon@madetech.com>
2022-08-15 12:19:59 +0100

    added tests and new upload-photo-ID page

--- routes/router.js
+++ routes/router.js
@@ -12,4 +12,8 @@ router.get('/what-is-your-name', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/what-is-your-name.html'))
 })
 
+router.get('/upload-photo-ID', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/upload-photo-ID.html'))
+})
+
 module.exports = router
