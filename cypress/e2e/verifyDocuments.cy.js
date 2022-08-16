e1c53afeb049f714a7e56327d38ca9939cdfc17a
Donny <donny.hyon@madetech.com>
2022-08-16 19:58:12 +0100

    updated tests to pass

--- cypress/e2e/verifyDocuments.cy.js
+++ cypress/e2e/verifyDocuments.cy.js
@@ -1,26 +1,26 @@
 describe('verify documents', () => {
-    // it('lands on the page', () => {
-    //   cy.visit('http://localhost:3000/verify-documents')
-    // })
+    it('lands on the page', () => {
+      cy.visit('http://localhost:3000/verify-documents')
+    })
     
     it('lets you move to next page- upload a video page', () => {
       cy.visit('http://localhost:3000/verify-documents')
-      cy.get('[type="radio"]').check('Upload-a-video')
+      cy.get('[type="radio"]').check("Upload a video")
       cy.get('#govuk-button-continue').click()
       cy.url().should('include', '/upload-a-video')
     })
 
-    // it('lets you move to next page- arrange-virtual-meeting', () => {
-    //   cy.visit('http://localhost:3000/verify-documents')
-    //   cy.get('#Arrange a virtual meeting').check()
-    //   cy.get('#govuk-button-continue').click()
-    //   cy.url().should('include', '/arrange-virtual-meeting')
-    // })
+    it('lets you move to next page- arrange-virtual-meeting', () => {
+      cy.visit('http://localhost:3000/verify-documents')
+      cy.get('[type="radio"]').check("Arrange a virtual meeting")
+      cy.get('#govuk-button-continue').click()
+      cy.url().should('include', '/arrange-virtual-meeting')
+    })
 
-    // it('lets you move to next page- arrange-face-to-face', () => {
-    //   cy.visit('http://localhost:3000/verify-documents')
-    //   cy.get('#Arrange a face to face meeting').check()
-    //   cy.get('#govuk-button-continue').click()
-    //   cy.url().should('include', '/arrange-face-to-face')
-    // })
+    it('lets you move to next page- arrange-face-to-face', () => {
+      cy.visit('http://localhost:3000/verify-documents')
+      cy.get('[type="radio"]').check("Arrange a face to face meeting")
+      cy.get('#govuk-button-continue').click()
+      cy.url().should('include', '/arrange-face-to-face')
+    })
   })
