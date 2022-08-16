fea7c6871683368d573504c5daf018cc86bf9736
Donny <donny.hyon@madetech.com>
2022-08-16 18:10:50 +0100

    pages work but test dont

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
 
 const routes = require('./routes/router')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+ extended: true
+}))
 
 app.use(express.static(path.join(__dirname, './public')))
 
