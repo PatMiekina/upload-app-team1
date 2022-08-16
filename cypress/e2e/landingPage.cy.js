82ea458c6e8df73f390fe88bbea072a01686c7c1
Donny <donny.hyon@madetech.com>
2022-08-16 13:13:51 +0100

    reinstated tests

---
+++ cypress/e2e/landingPage.cy.js
@@ -0,0 +1,10 @@
+describe('start page', () => {
+    it('lands on the page', () => {
+      cy.visit('http://localhost:3000')
+    })
+
+    it('lets you start the process', () => {
+        cy.get('.govuk-button--start').click()
+        cy.url().should('include', '/what-is-your-name')
+    })
+})
